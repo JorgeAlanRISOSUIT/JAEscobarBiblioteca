53975ff8336822d390dcfcca126139deca85a919
Jorge Alan Escobar Flores <rygenkio.minstar45@gmail.com>
2024-06-30 19:34:36 -0600

    Nuevos Adelantos
    Se ha agregar nuevos archivos para la capa de negocio

--- BusinessLayer/Autor.cs
+++ BusinessLayer/Autor.cs
@@ -12,12 +12,26 @@ namespace BusinessLayer
         {
 			try
 			{
+				ModelLayer.Autor model = new ModelLayer.Autor { Autores = new List<ModelLayer.Autor>() };
 				using (DataLayer.JAEscobarLibrosEntities context = new DataLayer.JAEscobarLibrosEntities())
 				{
 					List<DataLayer.ObtenerAutores_Result> result = context.ObtenerAutores().ToList();
 					if(result.Count > 0)
 					{
-
+						foreach (DataLayer.ObtenerAutores_Result item in result)
+						{
+							ModelLayer.Autor autores = new ModelLayer.Autor
+							{
+								IdAutor = item.IdAutor,
+								Nombre = item.Nombre,
+							};
+							model.Autores.Add(autores);
+						}
+						return (true, string.Empty, null, model);
+					}
+					else
+					{
+						return (false, "No existen autores", null, null);
 					}
 				}
 			}
---
+++ BusinessLayer/Editorial.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+	public class Editorial
+	{
+		public static (bool, string, Exception, ModelLayer.Editorial) GetEditoriales()
+		{
+			try
+			{
+				ModelLayer.Editorial model = new ModelLayer.Editorial { Editoriales = new List<ModelLayer.Editorial>() };
+				using (DataLayer.JAEscobarLibrosEntities context = new DataLayer.JAEscobarLibrosEntities())
+				{
+					List<DataLayer.ObtenerEditoriales_Result> editorial =  context.ObtenerEditoriales().ToList();
+					if(editorial.Count > 0)
+					{
+						foreach (DataLayer.ObtenerEditoriales_Result item in editorial)
+						{
+							ModelLayer.Editorial objEditorial = new ModelLayer.Editorial
+							{
+								IdEditorial = item.IdEditorial,
+								Nombre = item.Nombre
+							};
+							model.Editoriales.Add(objEditorial);
+						}
+						return (true, string.Empty, null, model);
+					}
+					else
+					{
+						return (false, "No hay Editoriales Disponibles", null, null);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				return (false, ex.Message, ex, null);
+			}
+		}
+	}
+}
