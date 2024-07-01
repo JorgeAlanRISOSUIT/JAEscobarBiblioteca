277d7e9a1eecca580955c892c00f3b1483b9cb6b
Jorge Alan Escobar Flores <rygenkio.minstar45@gmail.com>
2024-06-30 19:59:24 -0600

    Nuevos Controladores
    Se han agregado nuevos controladores a la API

---
+++ LibroAPI/Controllers/AutorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+using System.Web.Security;
+
+namespace LibroAPI.Controllers
+{
+	public class AutorController : ApiController
+	{
+		[HttpGet]
+		[Route("Autores")]
+		public HttpResponseMessage ObtenerAutores()
+		{
+			ModelLayer.ResultDTO modelDTO = new ModelLayer.ResultDTO();
+			var result = BusinessLayer.Autor.GetAutores();
+			if (result.Item1)
+			{
+				modelDTO.Success = true;
+				modelDTO.Objects = new List<object>();
+				foreach (ModelLayer.Autor item in result.Item4.Autores)
+				{
+					modelDTO.Objects.Add(item);
+				}
+				return Request.CreateResponse(HttpStatusCode.OK, modelDTO);
+			}
+			else
+			{
+				modelDTO.Success = false;
+				modelDTO.Message = result.Item2;
+				modelDTO.Error = result.Item3;
+				return Request.CreateResponse(HttpStatusCode.BadRequest, modelDTO);
+			}
+		}
+	}
+}
---
+++ LibroAPI/Controllers/EditorialController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LibroAPI.Controllers
+{
+	public class EditorialController : ApiController
+	{
+		[HttpGet]
+		[Route("Editoriales")]
+		public HttpResponseMessage ObtenerEditoriales()
+		{
+			ModelLayer.ResultDTO resultDTO = new ModelLayer.ResultDTO();
+			var result = BusinessLayer.Editorial.GetEditoriales();
+			if (result.Item1)
+			{
+				resultDTO.Success = true;
+				resultDTO.Objects = new List<object>();
+				foreach (ModelLayer.Editorial item in result.Item4.Editoriales)
+				{
+					resultDTO.Objects.Add(item);
+				}
+				return Request.CreateResponse(HttpStatusCode.OK, resultDTO);
+			}
+			else
+			{
+				resultDTO.Success = false;
+				resultDTO.Message = result.Item2;
+				resultDTO.Error = result.Item3;
+				return Request.CreateResponse(HttpStatusCode.BadRequest, resultDTO);
+			}
+		}
+	}
+}
